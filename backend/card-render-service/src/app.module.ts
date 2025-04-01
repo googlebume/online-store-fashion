c6146f32b08913c2fc3c19f29eb20c74ba9a0be0
googlebume <duzasergej@gmail.com>
2025-04-01 15:55:26 +0300

    Delete unnecessary files. Slightly optimised

--- backend/card-render-service/src/app.module.ts
+++ backend/card-render-service/src/app.module.ts
@@ -2,13 +2,10 @@ import { Module } from '@nestjs/common';
 import { CardsRenderController } from './cards-render/cards-render.controller';
 import { CardsRenderService } from './cards-render/cards-render.service';
 import { CardsRenderModule } from './cards-render/cards-render.module';
-import { FiltersHandlerModule } from './filters-handler/filters-handler.module';
-import { FiltersHandlerController } from './filters-handler/filters-handler.controller';
-import { FiltersHandlerService } from './filters-handler/filters-handler.service';
 
 @Module({
-  imports: [CardsRenderModule, FiltersHandlerModule],
-  controllers: [CardsRenderController, FiltersHandlerController],
-  providers: [CardsRenderService, FiltersHandlerService],
+  imports: [CardsRenderModule, ],
+  controllers: [CardsRenderController, ],
+  providers: [CardsRenderService, ],
 })
 export class AppModule { }
