1f7e5b33ab1213088a9a0c6dbd6942e2464d8b85
googlebume <duzasergej@gmail.com>
2025-07-04 16:11:25 +0300

    Added pictures to the list

--- services/admin/src/utils/constants/administrationList.ts
+++ services/admin/src/utils/constants/administrationList.ts
@@ -1,10 +1,14 @@
 import { AsideSectionType } from '@packages/shared/src/utils/types/renderComponents.type'
 
+import fileDocument from '../../../../../packages/shared/src/assets/images/icons/analytics.png' //'@packages/shared/src/assets/icons/file-document.png'
+import tag from '../../../../../packages/shared/src/assets/images/icons/tag.png'
+import userIcon from '../../../../../packages/shared/src/assets/images/icons/userIcon.png'
+
 export const administrationListItems: AsideSectionType = {
     title: 'administration',
     links: [
-        { href: "#", text: "Користувачі", icon: '#' },
-        { href: "#", text: "Товари", icon: '#' },
-        { href: "#", text: "Аналітика", icon: '#' },
+        { href: "#", text: "Користувачі", icon: userIcon },
+        { href: "#", text: "Товари", icon: tag },
+        { href: "#", text: "Аналітика", icon: fileDocument },
     ]
 }
