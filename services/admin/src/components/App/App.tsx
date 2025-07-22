98ba2138837fa6689d54a2a3c5a74f790317ce6d
Serhii Duzia <duzasergej@gmail.com>
2025-07-22 11:18:36 +0300

    Vidalennya component AsideAdmin

--- services/admin/src/components/App/App.tsx
+++ services/admin/src/components/App/App.tsx
@@ -2,14 +2,15 @@ import React, {useState} from 'react';
 import cl from './App.module.scss'
 import {Link, Outlet} from "react-router-dom";
 import {deepMerge} from '@packages/shared/src/utils/deepMerge'
+import { administrationListItems } from '@/utils/constants/administrationList';
 
-import AsideAdmin from '../AsideAdmin';
+import AsideList from '@packages/shared/src/components/AsideList';
 
 export const App = () => {
     // deepMerge()
     return (
         <div className={cl.wrapper}>
-            <AsideAdmin />
+            <AsideList section={ administrationListItems }/>
             <div className={cl.content}>
                     <Outlet />
             </div>
