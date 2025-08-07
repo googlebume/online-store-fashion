717e152f103f90dda176a16a12fe4e368a5fa854
Serhii Duzia <duzasergej@gmail.com>
2025-08-07 16:49:30 +0300

    Removing comments with obsolete code

--- backend/database/src/repositories/product.repository.ts
+++ backend/database/src/repositories/product.repository.ts
@@ -80,26 +80,6 @@ export class ProductRepository {
         return combined;
     }
 
-    //   async createProduct(name: string, price: number) {
-    //     return this.prisma.product.create({
-    //       data: { name, price },
-    //     });
-    //   }
-
-    //   async updateProduct(id: string, name?: string, price?: number) {
-    //     return this.prisma.product.update({
-    //       where: { id },
-    //       data: { name, price },
-    //     });
-    //   }
-
-    //   async deleteProduct(id: string) {
-    //     return this.prisma.product.delete({ where: { id } });
-    //   }
-
-
-
-
     changeArrToObj(arr) {
         const attributesObject = arr
             ? Object.fromEntries([
