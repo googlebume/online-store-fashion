1167da14d26c7a018e0bbf41ff4f7cecd72f921e
Serhii Duzia <duzasergej@gmail.com>
2025-08-10 18:59:20 +0300

    Improved typing of methods related to users in admin-service/users

--- backend/admin-service/src/users/users.service.ts
+++ backend/admin-service/src/users/users.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { lastValueFrom } from 'rxjs';
 import { databaseClient } from 'src/database.client';
+import type {User} from '@prisma/client';
 
 @Injectable()
 export class UsersService {
     async getAllUsers(): Promise<object[]> {
-        const allUsers = await lastValueFrom(databaseClient.send("get_all_users", {}))
+        const allUsers: User[] = await lastValueFrom(databaseClient.send("get_all_users", {}))
         return allUsers;
     }
 }
