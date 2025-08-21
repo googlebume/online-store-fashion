44c97fae7d2f9c76a123a2a486de79d8552858ae
Serhii Duzia <duzasergej@gmail.com>
2025-08-21 13:48:26 +0300

    Added some features for working with JWT

--- backend/services/auth-service/src/app.module.ts
+++ backend/services/auth-service/src/app.module.ts
@@ -5,6 +5,7 @@ import { LoginController } from './login/login.controller';
 import { LoginService } from './login/login.service';
 import { RegisterService } from './register/register.service';
 import { VerifyModule } from './verify/verify.module';
+import { JwtModule } from '@nestjs/jwt';
 
 
 @Module({
@@ -12,10 +13,17 @@ import { VerifyModule } from './verify/verify.module';
     RegisterModule,
     LoginModule,
     VerifyModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.SECRET || 'SECRET',
+      signOptions: {
+        expiresIn: '24h'
+      }
+    })
   ],
   controllers: [],
   providers: [
-    
+
   ],
 })
 export class AppModule { }
