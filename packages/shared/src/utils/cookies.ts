b945233f331a1e71e6f7bd10d4bf653535a13f12
Serhii Duzia <duzasergej@gmail.com>
2025-08-23 12:35:00 +0300

    Added object for working with cookies

--- packages/shared/src/utils/cookies.ts
+++ packages/shared/src/utils/cookies.ts
@@ -1,22 +1,23 @@
 type cookiesType<T = object> = {
+    name: string,
     data: string | number | T[],
-    path?: string
-    maxAge?: number
+    path?: string,
+    maxAge?: number,
 }
 
 class Cookies {
-    constructor() { }
-
     setCookies = (cookies?: cookiesType[]): void => {
-        cookies.forEach((cookie) => {
+        cookies?.forEach((cookie) => {
             this.setCookie(cookie)
         })
     }
 
     setCookie = (cookie?: cookiesType): void => {
+        if (!cookie) return;
         const setParam = this.setUnnecesaryParam(cookie)
-        document.cookie = `data=${setParam.data}; path=${setParam.path}; max-age=${setParam.maxAge};`
-
+        document.cookie = `${setParam.name}=${encodeURIComponent(
+            JSON.stringify(setParam.data)
+        )}; path=${setParam.path}; max-age=${setParam.maxAge}`
     }
 
     private setUnnecesaryParam = (cookie: cookiesType): cookiesType => {
@@ -28,8 +29,12 @@ class Cookies {
         }
         return cookie
     }
+
+    getCookie = (name: string): string | null => {
+        const cookies = document.cookie.split('; ')
+        const found = cookies.find(row => row.startsWith(name + '='))
+        return found ? decodeURIComponent(found.split('=')[1]) : null
+    }
 }
 
-
-
-
+export default Cookies
--- services/auth/src/components/LoginForm.tsx
+++ services/auth/src/components/LoginForm.tsx
@@ -1,108 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { api } from '@packages/shared/src/routes/api';
-// import { useNavigate } from 'react-router-dom';
-// import cl from '@/utils/styles/modules/RegisterForm.module.scss';
-
-// import InputData from '@packages/shared/src/components/UI/InputData/InputData';
-// import ErrorMassage from '@packages/shared/src/components/UI/ErrorMassage/ErrorMassage';
-// import SignWithGoogle from './UI/SignWithGoogle/SignWithGoogle';
-// import Devider from './UI/Devider/Devider';
-// import Terms from './UI/Terms/Terms';
-// import LoginLink from './UI/LoginLink/LoginLink';
-// import ButtonRegister from './UI/ButtonRegister/ButtonRegister';
-
-// import { useFetch } from '@packages/shared/src/utils/hooks/useFetch';
-// import { FormPropsType } from '@/utils/type/FormType';
-
-// type UserLoginType = {
-//     email: string;
-//     password: string;
-// };
-
-// const LoginForm = () => {
-//     const navigate = useNavigate();
-//     const [isError, setIsError] = useState(false);
-//     const [errorMessage, setErrorMessage] = useState('Помилка входу. Спробуйте ще раз');
-//     const [userData, setUserData] = useState<UserLoginType>({
-//         email: '',
-//         password: '',
-//     });
-
-//     const { response, error, isLoading, fetchData } = useFetch<UserLoginType, any>();
-
-//     const handleInputChange = (field: keyof UserLoginType, value: string) => {
-//         setUserData(prev => ({
-//             ...prev,
-//             [field]: value
-//         }));
-//         if (isError) setIsError(false);
-//     };
-
-//     const handleSubmit = (e: React.FormEvent) => {
-//         e.preventDefault();
-
-//         const form = e.currentTarget as HTMLFormElement;
-//         if (!form.checkValidity()) {
-//             form.reportValidity();
-//             return;
-//         }
-
-//         fetchData({
-//             method: 'POST',
-//             port: 4004,
-//             url: 'login',
-//             body: userData,
-//             event: 'login',
-//         });
-//     };
-
-//     useEffect(() => {
-//         if (response?.success) {
-//             navigate(`/${api}/shop`, {
-//                 state: { userData: response.userData }
-//             });
-//         }
-
-//         if (response?.success === false || error) {
-//             setIsError(true);
-//             setErrorMessage(response?.message || error?.message || 'Помилка входу');
-//         }
-//     }, [response, error]);
-
-//     return (
-//         <div className={cl.formContainer}>
-//             <SignWithGoogle />
-//             <Devider />
-//             <form className={cl.form} onSubmit={handleSubmit}>
-//                 <InputData
-//                     type="email"
-//                     id="email"
-//                     placeholder="your@email.com"
-//                     label="Електронна пошта"
-//                     required
-//                     name="email"
-//                     onInput={(val) => handleInputChange('email', val)}
-//                 />
-//                 <InputData
-//                     type="password"
-//                     id="password"
-//                     placeholder="Не менше 8 символів"
-//                     label="Пароль"
-//                     required
-//                     name="password"
-//                     onInput={(val) => handleInputChange('password', val)}
-//                 />
-//                 <Terms />
-//                 <ButtonRegister text={isLoading ? 'Зачекайте...' : 'Увійти'} />
-//                 {isError && <ErrorMassage massage={errorMessage} />}
-//             </form>
-
-//             <LoginLink type="login" onClick={e => navigate(`/${api}/register`)} />
-//         </div>
-//     );
-// };
-
-// export default LoginForm;
 import React, { useState, useEffect } from 'react';
 import { api } from '@packages/shared/src/routes/api';
 import { useNavigate } from 'react-router-dom';
@@ -116,6 +11,8 @@ import Terms from './UI/Terms/Terms';
 import LoginLink from './UI/LoginLink/LoginLink';
 import SubmitButton from '@packages/shared/src/components/UI/SubmitButton/SubmitButton';
 import { useFetch } from '@packages/shared/src/utils/hooks/useFetch';
+
+import Cookies from '@packages/shared/src/utils/cookies'
 
 type UserLoginType = {
   email: string;
@@ -189,21 +86,6 @@ const LoginForm = () => {
       }
     }, [response]);
 
-    // useEffect(() => {
-    //     if (isSubmit) {
-    //       console.log("Відправлено   ", userData)
-    //       fetchData({
-    //         method: 'POST',
-    //         port: 4004,
-    //         url: 'login/init',
-    //         body: {
-    //           email: userData.email,
-    //           password: userData.password,
-    //         },
-    //       });
-    
-    //     }
-    //   }, [isSubmit]);
 
   return (
     <form className={cl.formContainer} onSubmit={handleSubmit}>
