db214623692a00e01010b657c313c007008cb3f7
Serhii Duzia <duzasergej@gmail.com>
2025-08-23 17:22:13 +0300

    add jwt-auth guard

--- backend/packages/shared/src/common/guards/jwt-auth.guard.ts
+++ backend/packages/shared/src/common/guards/jwt-auth.guard.ts
@@ -1,18 +1,34 @@
-// import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-// import { Observable } from 'rxjs';
-// import { Reflector } from '@nestjs/core';
+import { Injectable, CanActivate, ExecutionContext, HttpException, UnauthorizedException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 
-// @Injectable()
-// export class Guard implements CanActivate {
-//     constructor(private readonly reflector: Reflector) {}
+@Injectable()
+export class Guard implements CanActivate {
+    constructor(
+        private readonly reflector: Reflector,
+        private readonly jwtService: JwtService,
+    ) { }
 
-//     canActivate(context: ExecutionContext):  boolean | Promise<boolean> | Observable<boolean> {
-//         const response = context.switchToHttp().getResponse()
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const response = context.switchToHttp().getResponse()
 
-//         try {
-//             const authHeader = response.headers.autorization
-//         } catch (error) {
+        try {
+            const authHeader = response.headers.autorization;
+            const { bearer, token } = authHeader.split(' ');
+            if (bearer !== 'Bearer' || !token) {
+                throw new UnauthorizedException({ message: 'Невірний токен' });
+            }
+
+            const verified = this.jwtService.verify(token);
+            response.user = verified;
+            if (!verified) {
+                throw new UnauthorizedException({ message: 'Застарілий токен' });
+            }
             
-//         }
-//     }
-// }
+            return true
+        } catch (error) {
+            throw new UnauthorizedException({ message: 'Не авторизований' });
+        }
+    }
+}
