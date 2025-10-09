c1eb77f5e0a321ea878c652424dea5b759e5ea27
Serhii Duzia <duzasergej@gmail.com>
2025-10-09 12:59:30 +0300

    Replaced the phone field with the email field in the order table schema

---
+++ backend/services/database/prisma/migrations/20251009095842_replaced_the_phone_field_with_the_email_field_in_the_order_table_schema/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `phone` on the `Order` table. All the data in the column will be lost.
+  - Added the required column `email` to the `Order` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Order" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "total" REAL NOT NULL,
+    "deliveryMethod" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Order" ("address", "createdAt", "deliveryMethod", "id", "status", "total", "updatedAt", "userId") SELECT "address", "createdAt", "deliveryMethod", "id", "status", "total", "updatedAt", "userId" FROM "Order";
+DROP TABLE "Order";
+ALTER TABLE "new_Order" RENAME TO "Order";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
